6cf89bc52c7e7932ef376bb7599dc12d68aea950
Permadi Eka Permana <35985549+permadiekapermana@users.noreply.github.com>
2021-01-13 09:32:53 +0700

    Update mineSweeper.java

--- mineSweeper.java
+++ mineSweeper.java
@@ -17,36 +17,75 @@ public class mineSweeper {
 
         String[][] huruf = {i1, i2, i3, i4, i5};
 
+        int kanan = 0, atas = 0, kiri = 0, bawah = 0, atasKiri = 0, atasKanan = 0, bawahKiri = 0, bawahKanan = 0, jumlah = 0;
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 3; j++) {
-                if (huruf[i][j].equals(".")){
-                    if (j+1 <huruf[i].length && i+1 <huruf[j].length) {
+                if (huruf[i][j].equals(".")) {
+                    if (j<2) {
                         if (huruf[i][j+1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i-1][j+1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i+1][j+1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i+1][j].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i+1][j-1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i][j-1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i-1][j-1].equals("*")){
-                            System.out.print("1 ");
-                        } else if (huruf[i-1][j].equals("*")){
-                            System.out.print("1 ");
-                        } else {
-                            System.out.print(huruf[i][j]+ " ");
+                            kanan++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
                         }
                     }
-                } else if (huruf[i][j].equals("*")) {
-                    System.out.print(huruf[i][j]+ " ");
+                    if (i<5 && i>=0) {
+                        if (huruf[i+1][j].equals("*")) {
+                            bawah++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (i>1) {
+                        if (huruf[i-1][j].equals("*")) {
+                            atas++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (j>=1) {
+                        if (huruf[i][j-1].equals("*")) {
+                            kiri++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (i>=1 && j>0) {
+                        if (huruf[i-1][j-1].equals("*")) {
+                            atasKiri++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (i>=1 && j<2) {
+                        if (huruf[i-1][j+1].equals("*")) {
+                            atasKanan++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (j>=1 && i<=3) {
+                        if (huruf[i+1][j-1].equals("*")) {
+                            bawahKiri++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    if (j<2 && i<4) {
+                        if (huruf[i+1][j+1].equals("*")) {
+                            bawahKanan++;
+                            jumlah = atas+kiri+bawah+kanan+atasKiri+atasKanan+bawahKiri+bawahKanan;
+                        }
+                    }
+                    System.out.print(jumlah+ " ");
+
+                    kanan=0;
+                    kiri=0;
+                    bawah=0;
+                    atas=0;
+                    atasKiri=0;
+                    atasKanan=0;
+                    bawahKiri=0;
+                    bawahKanan=0;
+                }
+                else if (huruf[i][j].equals("*")) {
+                    System.out.print(huruf[i][j]+" ");
                 }
             }
             System.out.println();
         }
-
     }
 }
